3629bc0d17414bada35207181ebd7dbee32a1b83
dchernik <dchernik@msn.com>
2015-07-17 17:49:21 +0000

    Add Dash Insert II solution and CoderByte version.

--- medium/dash-insert-II-coder-byte-special.rb
+++ medium/dash-insert-II-coder-byte-special.rb
@@ -5,31 +5,29 @@ even numbers in str. For example: if str is 4546793 the output should be
 454*67-9-3. Don't count zero as an odd or even number. 
 =end
 def is_even?(n_as_s)
-  n = n_as_s.to_i(10)
-  n != 0 && n % 2 == 0 # n != 0 && n.even? 
+  n = n_as_s.to_i
+  n != 0 && n.even? 
 end
 
 def is_odd?(n_as_s)
-  n = n_as_s.to_i(10)
-  n != 0 && n % 2 != 0 # n.odd?
+  n = n_as_s.to_i
+  n.odd?
 end
 
 def DashInsertII(num)
-  answ, str = '', num.to_s(10)
+  answ, str = '', num.to_s
   
-  # go through all digits except for the last one (so we don't have to 
-  # check nill for oddness)
   for i in 0...str.length - 1
-    answ << str[i]
-    if is_even?(str[i]) && is_even?(str[i+1])
-      answ << '*'
-    elsif is_odd?(str[i]) && is_odd?(str[i+1])
-      answ << '-'
+    curr_char = str[i].chr
+    next_char = str[i+1].chr
+    answ += curr_char
+    if is_even?(curr_char) && is_even?(next_char)
+      answ += '*'
+    elsif is_odd?(curr_char) && is_odd?(next_char)
+      answ += '-'
     end
   end
-
-  # append last digit and return
-  answ + str[-1]
+  answ + str[-1].chr
 end
 
 # keep this function call here 
